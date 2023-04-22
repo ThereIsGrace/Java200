188d96257a58a9881239042f70dd3b0fea42a471
ThereIsGrace <universe2879@gmail.com>
2023-04-22 22:51:43 +0900

    쿼리 테스트

---
+++ kr/co/infopub/chapter/s183/test/DepartmentTest02.java
@@ -0,0 +1,24 @@
+package kr.co.infopub.chapter.s183.test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import kr.co.infopub.chapter.s183.dto.DepartmentDto;
+import kr.co.infopub.chapter.s183.model.EmployeeDAO;
+import kr.co.infopub.chapter.s183.util.EmpUtil;
+
+public class DepartmentTest02 {
+	public static void main(String[] args) {
+		EmployeeDAO ddao = new EmployeeDAO();
+		try {
+			List<DepartmentDto> lists = ddao.findAllDepartments2();
+			System.out.println("Results--------------------");
+			for (DepartmentDto dd: lists) {
+				System.out.println(EmpUtil.tname(dd));
+			}
+			System.out.printf("모든 부서 수 : %d개\n",lists.size());
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+}
